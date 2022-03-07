a32026c7e85b996b746b4e011fbe853f9a042bd7
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2022-03-07 10:15:25 +0300

    lab_03

---
+++ lab_03/part_02/md.h
@@ -0,0 +1,9 @@
+#ifndef MD_H_
+#define MD_H_
+
+extern char *md1_data;
+
+extern char *md1_proc(void);
+
+
+#endif // MD_H_
---
+++ lab_03/r/part_02/md.h
@@ -0,0 +1,12 @@
+#ifndef __MD1_H__
+#define __MD1_H__
+
+extern char *md1_data;
+
+extern char *md1_proc(void);
+
+extern char *md1_noexport(void);
+
+static char *md1_local(void);
+
+#endif
