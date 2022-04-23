9e759246822b00f9047e44f309415849569f9e89
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2022-04-23 18:57:31 +0300

    lab5

--- lab_05/src/prog_01.c
+++ lab_05/src/prog_01.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-#define OK 0
-#define BUF_SIZE 20
-#define VALID_READED 1
-
-#define FILE_NAME "data/alphabet.txt"
-#define SPEC "%c"
-
 int main(void)
 {
-    int fd = open(FILE_NAME, O_RDONLY);
+	// have kernel open connection to file alphabet.txt
+    int fd = open("alphabet.txt", O_RDONLY);
 
+	// create two C I/O buffered streams using the above connection 
     FILE *fs1 = fdopen(fd, "r");
-    char buff1[BUF_SIZE];
-    setvbuf(fs1, buff1, _IOFBF, BUF_SIZE);
+    char buff1[20];
+    setvbuf(fs1, buff1, _IOFBF, 20);
 
     FILE *fs2 = fdopen(fd, "r");
-    char buff2[BUF_SIZE];
-    setvbuf(fs2, buff2, _IOFBF, BUF_SIZE);
+    char buff2[20];
+    setvbuf(fs2, buff2, _IOFBF, 20);
 
+	// read a char & write it alternatingly from fs1 and fs2
     int flag1 = 1, flag2 = 2;
-    while (flag1 == VALID_READED || flag2 == VALID_READED)
+    while (flag1 == 1 || flag2 == 1)
     {
         char c;
 
-        if ((flag1 = fscanf(fs1, SPEC, &c)) == VALID_READED)
+        if ((flag1 = fscanf(fs1, "%c", &c)) == 1)
         {
-            fprintf(stdout, SPEC, c);
+            fprintf(stdout, "%c", c);
         }
 
-        if ((flag2 = fscanf(fs2, SPEC, &c)) == VALID_READED)
+        if ((flag2 = fscanf(fs2, "%c", &c)) == 1)
         {
-            fprintf(stdout, SPEC, c);
+            fprintf(stdout, "%c", c);
         }
     }
 
-    return OK;
+    return 0;
 }
+
+/*
+Функция fdopen связывает поток с существующим описателем файла fildes. Режим mode потока (одно из следующих значений: "r", "r+", "w", "w+", "a", "a+") 
+должен быть совместим с режимом описателя файла. Указатель файловой позиции в новом потоке принимает значение, равное значению fildes, 
+а указатели ошибок и конца файла по значению равны нулю. Режимы "w" или "w+" не "урезают" файл. Описатель файла не скопирован и будет закрыт,
+ когда поток, созданный fdopen, закрывается. Результат применения fdopen в общем объекте памяти неопределен.
+
+Существует три типа буферизации: нулевая буферизация (ее отсутствие), буферизация блока и буферизация строки.
+Функция setvbuf может быть использована над любым открытым потоком для изменения типа буферизации. Параметр mode должен быть одним из трех следующих макросов:
+
+_IONBF
+(отключить буферизацию);
+_IOLBF
+(строчная буферизация);
+_IOFBF
+(блочная буферизация).
+*/
--- lab_05/src/prog_01_thread.c
+++ lab_05/src/prog_01_thread.c
@@ -26,7 +26,7 @@ void *run_buffer(void *args)
     }
 
     // fprintf(stdout, "\n======== LEAVE RUN_BUFFER ==========");
-    return NULL;
+
 }
 
 int main(void)
@@ -64,3 +64,64 @@ int main(void)
 
     return OK;
 }
+
+/*
+#include <stdio.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void *thread_func(void *args)
+{
+    int flag = 1;
+    FILE *fs = (FILE *)args;
+
+    while (flag == 1)
+    {
+        char c;
+        if ((flag = fscanf(fs, "%c\n", &c)) == 1)
+        {
+            fprintf(stdout, "Additional thread read: %c\n", c);
+        }
+    }
+}
+
+int main(void)
+{
+    setbuf(stdout, NULL);
+
+    pthread_t thread;
+    int fd = open("alphabet.txt", O_RDONLY);
+
+    FILE *fs1 = fdopen(fd, "r");
+    char buff1[20];
+    setvbuf(fs1, buff1, _IOFBF, 20);
+
+    FILE *fs2 = fdopen(fd, "r");
+    char buff2[20];
+    setvbuf(fs2, buff2, _IOFBF, 20);
+
+    if (pthread_create(&thread, NULL, thread_func, (void *)fs2) != 0)
+	{
+		perror("Error in pthread_create\n");
+		return -1;
+	}
+
+    int flag = 1;
+    while (flag == 1)
+    {
+        char c;
+        if ((flag = fscanf(fs1, "%c\n", &c)) == 1)
+        {
+            fprintf(stdout, "Main thread read: %c\n", c);
+        }
+    }
+	// The pthread_join() function suspends processing of the calling thread until the target thread completes.
+	// 2 arg - void **status
+    pthread_join(thread, NULL);
+    //wait(NULL);
+
+    return 0;
+}
+*/
